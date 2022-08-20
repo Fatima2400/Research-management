4482944fcb651fe81c425b949f007e35d66f9cee
Fatima Deshmukh <96168561+Fatima2400@users.noreply.github.com>
2022-08-20 15:30:43 +0530

    update

---
+++ src/App.vue
@@ -0,0 +1,76 @@
+<script>
+import Header from "./components/Header.vue"
+import AddNew from "./components/AddNew.vue";
+import Reports from "./components/Reports.vue"
+//import Charts from "./components/Charts.vue"
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    AddNew,
+    Reports,
+   
+  },
+ 
+  data(){
+    return {
+        tasks:[],
+        
+    }
+},
+methods:{
+newTask(task){
+this.tasks= [...this.tasks, task]
+
+}
+},
+created(){
+   this.tasks = [
+       {id:1,
+       publish: "U998989",
+       name: "Viraj Lagad ",
+       year: 2022,
+indexing: 1,
+journal:'Physics',
+       },
+       {id:2,
+       publish: "U37878",
+       name: "Anirudh Murali",
+       year: 2023,
+indexing: 2,
+journal:'Data Science',
+       }
+
+   ]
+   
+}
+}
+</script>
+
+<template>
+  
+   
+
+    <div class="wrapper">
+      <Header  />
+      <AddNew @new-task='newTask'  />
+     <div class="rep">
+<Reports :tasks="tasks"/>
+</div>
+
+
+ 
+      
+    </div>
+  
+
+  
+</template>
+
+<style>
+
+
+</style>
+
+
---
+++ src/components/Header.vue
@@ -0,0 +1,75 @@
+<script>
+
+export default {
+    name: 'Header',
+
+}
+</script>
+
+
+
+<template>
+<div class="container">
+<h1>Academic Research</h1>
+<div class="bttn">
+    <button  class="btn rep">Generate Report</button>
+    <button  class="btn res">Add Research</button>
+</div>
+<!-- <div class="filt">
+   
+    <select name="Department" class="select" id="dep">
+        <option value="a">Department</option>
+        <option value="b">b</option>
+        <option value="c">c</option>
+    </select>
+
+    <select name="FY" class="select" id="fy">
+        <option value="a">FY</option>
+        <option value="b">b</option>
+        <option value="c">c</option>
+    </select>
+
+    <select name="div A" class="select" id="div-A">
+        <option value="a">Div A</option>
+        <option value="b">b</option>
+        <option value="c">c</option>
+    </select>
+    <select name="Academic-year" class="select" id="year">
+        <option value="a">Academic-year</option>
+        <option value="b">b</option>
+        <option value="c">c</option>
+    </select>
+</div> -->
+</div>
+</template>
+
+<style scoped>
+.select{
+    padding: 12px 20px;
+    margin: 2%;
+    font-weight: bold;
+    font-size: 15px;
+    background-color: 	 #ccd6f6;
+    border-radius: 5px;
+}
+.bttn{
+    display: flex;
+    justify-content: end;
+}
+.btn{
+    padding: 12px 20px;
+    font-size: 16px;
+    margin: 5px;
+    border-radius: 5px;
+}
+.rep{
+    background-color: red;
+    color: white;
+    border-color: transparent;
+}
+.res{
+    background-color: white;
+    border-color: black;
+}
+</style>
+
